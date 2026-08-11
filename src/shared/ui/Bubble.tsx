6c0610931584fc149dfad0af5e6fb3dfedb51c0f
IvanIlinov <i.am.ivan.ilinov@gmail.com>
2026-08-11 22:30:31 +0300

    upd: orange  color to #f7ce9c

--- src/shared/ui/Bubble.tsx
+++ src/shared/ui/Bubble.tsx
@@ -11,7 +11,7 @@ const MEMORY_COLOR_HEX: Record<MemoryColor, string> = {
   mint: "#86EFAC",
   lime: "#9CF7A8",
   amber: "#f6f79c",
-  orange: "#FB923C",
+  orange: "#f7ce9c",
   coral: "#F87171",
   red: "#EF4444",
 };
